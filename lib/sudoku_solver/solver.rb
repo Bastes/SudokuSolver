340b83bdbcb7f2095f00f297f35a89cb6a6f3886
Michel Belleville <michel.belleville@gmail.com>
2010-01-29 17:53:17 +0100

    Testing a bit harder, but still not much.

--- lib/sudoku_solver/solver.rb
+++ lib/sudoku_solver/solver.rb
@@ -9,7 +9,7 @@ module SudokuSolver
       possibilities = (1..width)
       grid = CellularMap::Map.new[size, size]
       grid.each { |c|
-        v = problem[c.x][c.y]
+        v = problem[c.y][c.x]
         c.content = v ? [v] : (1..width).to_a
       }
       self.reduce(grid, width, dimension, possibilities, size).
@@ -20,17 +20,19 @@ module SudokuSolver
 
     def self.reduce(grid, width, dimension, possibilities, size)
       grid.each { |c|
-        sx = c.x / dimension
-        sy = c.y / dimension
-        neighbouring_values = [
-          grid[size, c.y],
-          grid[c.x, size],
-          grid[sx..(sx + dimension - 1), sy..(sy + dimension - 1)] ].
-            inject([]) { |r, z| r + z.collect { |d|
-              (d == c) ? nil : d.content } }.compact.uniq
-        taken = neighbouring_values.reject { |r| r.length > 1 }.flatten
-        remaining = c.content - taken
-        c.content = remaining
+        unless c.content.length == 1
+          sx = c.x / dimension
+          sy = c.y / dimension
+          neighbouring_values = [
+            grid[size, c.y],
+            grid[c.x, size],
+            grid[sx..(sx + dimension - 1), sy..(sy + dimension - 1)] ].
+              inject([]) { |r, z| r + z.collect { |d|
+                (d == c) ? nil : d.content } }.compact.uniq
+          taken = neighbouring_values.reject { |r| r.length > 1 }.flatten
+          remaining = c.content - taken
+          c.content = remaining
+        end
       }
     end
   end
--- test/test_solver.rb
+++ test/test_solver.rb
@@ -8,4 +8,20 @@ class TestSolver < Test::Unit::TestCase
       assert_equal [[1]], solution
     }
   }
+
+  context('A very easy problem') {
+    setup {
+      @solution =
+        [ [ 1, 2, 3, 4],
+          [ 3, 4, 2, 1],
+          [ 2, 1, 4, 3],
+          [ 4, 3, 1, 2] ]
+      @problem = @solution.collect { |l| l.clone }
+      @problem[0][0] = nil
+    }
+    should('still be easy to solve') {
+      solved = SudokuSolver::Solver.solve(@problem)
+      assert_equal @solution, solved
+    }
+  }
 end
