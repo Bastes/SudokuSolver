dc97ec12cd571535bb91f1378da75e292bfc89fb
Michel Belleville <michel.belleville@gmail.com>
2010-02-01 17:37:49 +0100

    Refactoring before complexity.

--- lib/sudoku_solver/solver.rb
+++ lib/sudoku_solver/solver.rb
@@ -1,43 +1,54 @@
 require 'cellular_map'
 
 module SudokuSolver
-  module Solver
+  class Solver
     def self.solve(problem)
-      width = problem.length
-      dimension = Math.sqrt(width).to_i
-      size = (0..(width -1))
-      possibilities = (1..width)
-      grid = CellularMap::Map.new[size, size]
-      grid.each { |c|
-        v = problem[c.y][c.x]
-        c.content = v ? [v] : (1..width).to_a
-      }
-      self.reduce(grid, width, dimension, possibilities, size).
-        to_a.collect { |l| l.collect { |c| c.content.length == 1 ? c.content.first : nil } }
+      self.new(problem).solve
     end
 
-    protected
+    def grid
+      CellularMap::Map.new[@size, @size].each { |c|
+        v = @problem[c.y][c.x]
+        c.content = v ? [v] : @possibilities.to_a
+      }
+    end
 
-    def self.reduce(grid, width, dimension, possibilities, size)
+    def initialize(problem)
+      @problem = problem
+      @width = @problem.length
+      @dimension = Math.sqrt(@width).to_i
+      @size = (0..(@width -1))
+      @possibilities = (1..@width)
+    end
+
+    def solve
+      self.reduce(self.grid).to_a.collect { |l| l.collect { |c|
+        c.content.length == 1 ? c.content.first : nil } }
+    end
+
+    def reduce(grid)
       unsolved = grid.reject { |c| c.content.length == 1 }
       until unsolved.empty? do
-        changed = unsolved.reject { |c|
-          sx = c.x - c.x % dimension
-          sy = c.y - c.y % dimension
-          neighbouring_values = [
-            grid[size, c.y],
-            grid[c.x, size],
-            grid[sx..(sx + dimension - 1), sy..(sy + dimension - 1)] ].
-              inject([]) { |r, z| r + z.collect { |d|
-                (d == c) ? nil : d.content } }.compact.uniq
-          taken = neighbouring_values.reject { |r| r.length > 1 }.flatten
-          remaining = (c.content - taken).sort
-          (c.content == remaining) || ! (c.content = remaining)
-        }
+        changed = unsolved.reject { |c| restrict(c) }
         break unless changed.length > 0
         unsolved.reject! { |c| c.content.length == 1 }
       end
       grid
+    end
+
+    def restrict(cell)
+      grid = cell.map
+      sx = cell.x - cell.x % @dimension
+      sy = cell.y - cell.y % @dimension
+      neighbouring_values = [
+        grid[@size, cell.y],
+        grid[cell.x, @size],
+        grid[sx..(sx + @dimension - 1), sy..(sy + @dimension - 1)] ].
+          inject([]) { |r, z| r + z.collect { |d|
+            (d == cell) ? nil : d.content } }.compact.uniq
+      taken = neighbouring_values.reject { |r| r.length > 1 }.flatten
+      remaining = (cell.content - taken).sort
+      (cell.content == remaining) || ! (cell.content = remaining)
     end
   end
 end
