3e74714ee42ff85fa469ddd828ea0fec307efbda
Michel Belleville <michel.belleville@gmail.com>
2010-04-24 11:07:52 +0200

    Fixed a small mistake in valid problem testing

--- test/test_solver.rb
+++ test/test_solver.rb
@@ -19,7 +19,7 @@ class TestSolver < Test::Unit::TestCase
   } 
 
   context('The simplest problem in the world') {
-    setup { @problem = [[]] }
+    setup { @problem = [[nil]] }
     should('be easy to solve') {
       solution = SudokuSolver::Solver.solve(@problem)
       assert_equal [[1]], solution
